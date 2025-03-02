8189aac004e36a189fb28df695efde4570ab95df
wiktorwiktor12 <68821018+wiktorwiktor12@users.noreply.github.com>
2025-03-02 17:53:10 +0000

    add in proper image scaling
    i had a look at authui from win7, then realised that they did scaling before giving the image to dui, which is why on clh it looked ass

--- ConsoleLogonUI/util/util.cpp
+++ ConsoleLogonUI/util/util.cpp
@@ -5,38 +5,50 @@
 
 HBITMAP GetHBITMAPFromImageResource(int resourceID)
 {
-    auto foundresource = FindResourceW(duiManager::Get()->hInstance, MAKEINTRESOURCEW(resourceID), L"IMAGE");
-    if (!foundresource) return (HBITMAP)0;
-    DWORD imageSize = SizeofResource(duiManager::Get()->hInstance, foundresource);
-    const void* resourceData = ::LockResource(::LoadResource(duiManager::Get()->hInstance, foundresource));
+	static auto SHCreateStreamOnModuleResourceW = (HRESULT(WINAPI*)(HMODULE hModule, LPCWSTR pwszName, LPCWSTR pwszType, IStream * *ppStream))(GetProcAddress(LoadLibrary(L"shcore.dll"), (PCSTR)109));
 
-    HBITMAP hbitmap = 0;
+	IStream* Stream = 0;
+	HRESULT hr = SHCreateStreamOnModuleResourceW(duiManager::Get()->hInstance, MAKEINTRESOURCEW(resourceID), L"IMAGE", &Stream);
+	if (FAILED(hr))
+	{
+		return 0;
+	}
+	Gdiplus::GdiplusStartupInput gpStartupInput;
+	ULONG_PTR gpToken;
+	Gdiplus::GdiplusStartup(&gpToken, &gpStartupInput, NULL);
 
-    void* buffer = GlobalAlloc(GMEM_MOVEABLE,imageSize);
-    if (buffer)
-    {
-        void* pBuffer = ::GlobalLock(buffer);
-        memcpy(pBuffer, resourceData, imageSize);
-        IStream* pStream = NULL;
-        if (CreateStreamOnHGlobal(buffer, FALSE, &pStream) == S_OK)
-        {
-            Gdiplus::GdiplusStartupInput gpStartupInput;
-            ULONG_PTR gpToken;
-            Gdiplus::GdiplusStartup(&gpToken, &gpStartupInput, NULL);
+	Gdiplus::Bitmap* bitmap = Gdiplus::Bitmap::FromStream(Stream);
 
-            Gdiplus::Bitmap* bitmap = Gdiplus::Bitmap::FromStream(pStream);
+	HBITMAP hbitmap = 0;
 
-            if (bitmap)
-            {
-                bitmap->GetHBITMAP(Gdiplus::Color(255, 255, 255), &hbitmap);
-                pStream->Release();
-                delete bitmap;
-            }
-            Gdiplus::GdiplusShutdown(gpToken);
-        }
-        GlobalUnlock(buffer);
-        GlobalFree(buffer);
-    }
-    
-    return hbitmap;
+	if (bitmap)
+	{
+		UINT newWidth = GetSystemMetrics(SM_CXSCREEN);
+		UINT newHeight = GetSystemMetrics(SM_CYSCREEN);
+		Gdiplus::Bitmap bitmap2 = Gdiplus::Bitmap(newWidth,newHeight,PixelFormat32bppARGB);
+
+		Gdiplus::Graphics* graphic = Gdiplus::Graphics::FromImage(&bitmap2);
+		if (graphic)
+		{
+			graphic->SetInterpolationMode(Gdiplus::InterpolationMode::InterpolationModeHighQualityBicubic);
+
+			Gdiplus::ImageAttributes attributes;
+			attributes.SetWrapMode(Gdiplus::WrapMode::WrapModeTileFlipXY, 0xFF000000, 0);
+
+			UINT oldWidth = bitmap->GetWidth();
+			UINT oldHeight = bitmap->GetHeight();
+
+			graphic->DrawImage(bitmap, Gdiplus::Rect(0,0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN)), 0,0, bitmap->GetWidth(), bitmap->GetHeight(), Gdiplus::Unit::UnitPixel, &attributes, (Gdiplus::DrawImageAbort)0, 0);
+
+			bitmap2.GetHBITMAP(Gdiplus::Color(0xFF000000), &hbitmap);
+
+			delete graphic;
+		}
+
+		delete bitmap;
+	}
+	Stream->Release();
+	Gdiplus::GdiplusShutdown(gpToken);
+
+	return hbitmap;
 }
